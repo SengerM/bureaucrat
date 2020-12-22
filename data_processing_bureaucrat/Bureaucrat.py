7c2797bca3fc0006eb62936647f9706712fa8e44
Matias Senger <m.senger@hotmail.com>
2020-12-22 17:03:45 +0100

    method processed_by_script_dir_paht implemented

--- data_processing_bureaucrat/Bureaucrat.py
+++ data_processing_bureaucrat/Bureaucrat.py
@@ -7,14 +7,17 @@ import warnings
 class Bureaucrat:
 	"""
 	Naming convention:
-	- Stuff that ends in "path" is supposed to be/return an instance of Path
+	- Stuff that ends in "path" is supposed to be/return an instance of Path.
 	- Stuff that ends in "name" are strings.
 	"""
+	PROCESSED_DATA_DIRECTORY_PREFIX = 'processed_by_script_'
 	def __init__(self, measurement_base_path: str):
+		if ' ' in measurement_base_path:
+			warnings.warn(f'The <measurement_base_path> = "{measurement_base_path}" contains blank spaces. I can handle this, but it is better to aviod them.')
 		self._timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 		self._measurement_base_path =  Path(measurement_base_path)
 		self._processing_script_absolute_path = Path.cwd()/Path( __main__.__file__)
-		self._processed_data_subdir_name = f'processed_by_script_{self._processing_script_absolute_path.parts[-1].replace(".py","")}'
+		self._processed_data_subdir_name = f'{self.PROCESSED_DATA_DIRECTORY_PREFIX}{self._processing_script_absolute_path.parts[-1].replace(".py","")}'
 		
 		if not self._measurement_base_path.is_dir():
 			raise ValueError(f'Directory "{self._measurement_base_path}" does not exist.')
@@ -43,7 +46,9 @@ class Bureaucrat:
 		_ = self._measurement_base_path/Path(self._processed_data_subdir_name)
 		_.mkdir(exist_ok=True)
 		return _
-
-# ~ class ProcessingBureaucrat(Bureaucrat):
-	# ~ def __init__(self, measurement_base_path: str):
-		
+	
+	def processed_by_script_dir_path(self, script_name: str):
+		_ = self._measurement_base_path/Path(f'{self.PROCESSED_DATA_DIRECTORY_PREFIX}{script_name.replace(".py","")}')
+		if not _.is_dir():
+			warnings.warn(f'Directory with the output of script <{script_name}> "{_}" does not exist.')
+		return _
