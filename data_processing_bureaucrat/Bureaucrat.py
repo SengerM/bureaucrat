0bb8cc9669bba23a5dd4b8c499a93486af9598ad
Matias Senger <m.senger@hotmail.com>
2021-02-17 13:25:19 +0100

    New implementation for the TelegramProgressBar

--- data_processing_bureaucrat/Bureaucrat.py
+++ data_processing_bureaucrat/Bureaucrat.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import datetime
 import warnings
-from tqdm.contrib.telegram import tqdm
 import inspect
+import requests
 
 class Bureaucrat:
 	"""
@@ -88,8 +88,6 @@ using locals() which does exactly that.''')
 
 class TelegramProgressBar:
 	"""
-	Taken from here: # https://stackoverflow.com/a/26761413/8849755
-	
 	Usage example
 	-------------
 	
@@ -107,22 +105,80 @@ class TelegramProgressBar:
 			time.sleep(1)
 			pbar.update(1)
 	"""
-	def __init__(self, total, bureaucrat: Bureaucrat, telegram_token='923059887:AAGW18jNJOshNi83r0Y6JsfizCdrCi8ytZQ', telegram_chat_id='164530575', **kwargs):
-		BARRAENE = '\n'
-		self._total = total
+	def __init__(self, total: int, bureaucrat: Bureaucrat, telegram_token='923059887:AAGW18jNJOshNi83r0Y6JsfizCdrCi8ytZQ', telegram_chat_id='164530575', **kwargs):
 		self._telegram_token = telegram_token
 		self._telegram_chat_id = telegram_chat_id
-		self._description = bureaucrat.timestamp
+		self._bureaucrat = bureaucrat
+		if not isinstance(total, int):
+			raise TypeError(f'<total> must be an integer number, received {total} of type {type(total)}.')
+		self._total = total
 
 	def __enter__(self):
-		self._pbar = tqdm(
-			total = self._total, 
-			token = self._telegram_token, 
-			chat_id = self._telegram_chat_id,
-			desc = self._description,
-		)
-		return self._pbar
+		try:
+			response = self.send_message(f'Starting {self._bureaucrat.measurement_name}...')
+			self._message_id = response['result']['message_id']
+		except:
+			warnings.warn(f'Could not establish connection with Telegram to send the progress status.')
+		self._count = 0
+		self._start_time = datetime.datetime.now()
+		return self
 		
+	def update(self, count: int):
+		if not hasattr(self, '_count'):
+			raise RuntimeError(f'Before calling to <update> you should create a context using "with TelegramProgressBar(...) as pbar:".')
+		if not isinstance(count, int):
+			raise TypeError(f'<count> must be an integer number, received {count} of type {type(count)}.')
+		self._count += count
+		if hasattr(self, '_message_id'):
+			message_string = f'{self._bureaucrat.measurement_name}\n\n'
+			message_string += f'Started: {self._start_time.strftime("%Y-%m-%d %H:%M")}\n'
+			message_string += f'Expexted finish: {(self._start_time + (datetime.datetime.now()-self._start_time)/self._count*self._total).strftime("%Y-%m-%d %H:%M")}\n'
+			message_string += '\n'
+			message_string += f'{self._count}/{self._total} | {self._count/self._total*100:.2f} %'
+			try:
+				self.edit_message(
+					message_text = message_string,
+					message_id = self._message_id,
+				)
+			except KeyboardInterrupt:
+				raise KeyboardInterrupt()
+			except:
+				warnings.warn(f'Could not establish connection with Telegram to send the progress status.')
+	
 	def __exit__(self, exc_type, exc_value, exc_traceback):
-		self._pbar.__exit__(exc_type, exc_value, exc_traceback)
-		
+		now = datetime.datetime.now()
+		if hasattr(self, '_message_id'):
+			message_string = f'{self._bureaucrat.measurement_name}\n\n'
+			if self._count != self._total:
+				message_string += f'FINISHED WITHOUT REACHING 100 %\n\n'
+			message_string += f'Finished on {now.strftime("%Y-%m-%d %H:%M")}\n'
+			message_string += f'Total elapsed time: {(now-self._start_time)}\n'
+			try:
+				self.edit_message(
+					message_text = message_string,
+					message_id = self._message_id,
+				)
+			except:
+				warnings.warn(f'Could not establish connection with Telegram to send the progress status.')
+	
+	def send_message(self, message_text):
+		# https://core.telegram.org/bots/api#sendmessage
+		response = requests.get(
+			f'https://api.telegram.org/bot{self._telegram_token}/sendMessage',
+			data = {
+				'chat_id': self._telegram_chat_id,
+				'text': message_text,
+			}
+		)
+		return response.json()
+
+	def edit_message(self, message_text, message_id):
+		# https://core.telegram.org/bots/api#editmessagetext
+		requests.post(
+			f'https://api.telegram.org/bot{self._telegram_token}/editMessageText',
+			data = {
+				'chat_id': self._telegram_chat_id,
+				'text': message_text,
+				'message_id': str(message_id),
+			}
+		)
