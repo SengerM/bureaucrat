fb1592144bbcdd7ab6ab815b54cc2bd5614309e8
Matias Senger <m.senger@hotmail.com>
2022-03-11 10:11:06 +0100

    Added documentation

--- bureaucrat/Bureaucrat.py
+++ bureaucrat/Bureaucrat.py
@@ -5,14 +5,41 @@ import inspect
 
 class Bureaucrat:
 	"""
-	Naming convention:
-	- Stuff that ends in "path" is supposed to be/return an instance of Path.
+	A class to normalize and ease the handling of directories with data.
+	
+	Naming convention
+	-----------------
+	- Stuff that ends in "path" is supposed to return an instance of Path.
 	- Stuff that ends in "name" are strings.
 	"""
 	PROCESSED_DATA_DIRECTORY_PREFIX = ''
 	SCRIPT_SUCCESSFULLY_FINISHED_WITHOUT_ERRORS_FILE_FLAG_NAME = '.script_successfully_applied'
 	
 	def __init__(self, measurement_base_path: str, variables: dict = None, new_measurement=False):
+		"""Create an instance of `Bureaucrat`.
+		
+		Parameters
+		----------
+		measurement_base_path: str
+			Path to the directory that contains your measurement. Example:
+			```
+			measurement_base_path = '/path/to/my_measurement'
+			```
+		variables: ATTENTION HERE!
+			This is in some sense a hack for creating a backup of the script
+			that called this method. You always have to use this parameter
+			in the following way:
+			```
+			variables = locals()
+			```
+			I found no way to automatize and hide this, sorry.
+		new_measurement: bool, default `False`
+			If `False` then `measurement_base_path` must exist beforehand,
+			i.e. you will run this script on a measurement that was already
+			performed in the past, for example you will now analyze this 
+			data with another script. If `True` then a new directory
+			will be created.
+		"""
 		if variables is None:
 			raise ValueError(f'''<variables> must be a dictionary with the variables names and their values, so the Bureaucrat can keep a record in the processed data directory. The easy way is to call
 bureaucrat = Bureaucrat(
@@ -58,18 +85,24 @@ using locals() which does exactly that.''')
 	
 	@property
 	def measurement_base_path(self):
+		"""Returns the path to the directory where all data concerning this
+		measurement (this and other scripts) should be."""
 		return self._measurement_base_path
 	
 	@property
 	def measurement_name(self):
+		"""Returns a string with the measurement name."""
 		return self.measurement_base_path.parts[-1]
 	
 	@property
 	def this_run_timestamp(self):
+		"""Returns a string with a timestamp for the current run. This means
+		the timestamp when the current bureaucrat was created."""
 		return self._timestamp
 	
 	@property
 	def timestamp(self):
+		"""Same as `this_run_timestamp`."""
 		return self._timestamp
 	
 	@property
@@ -86,8 +119,7 @@ using locals() which does exactly that.''')
 	
 	@property
 	def processed_data_dir_path(self):
-		"""Returns the path to the sub directory where the current script
-		should store its data."""
+		"""Same as `current_script_output_directory_path`."""
 		_ = self._measurement_base_path/Path(self._processed_data_subdir_name)
 		_.mkdir(exist_ok=True)
 		return _
@@ -109,8 +141,7 @@ using locals() which does exactly that.''')
 		return self.processed_by_script_dir_path(script_name)
 	
 	def processed_by_script_dir_path(self, script_name: str):
-		"""Returns the path to the sub directory where `script_name` 
-		should have stored its data."""
+		"""Same as `other_script_output_directory_path`."""
 		_ = self._measurement_base_path/Path(f'{self.PROCESSED_DATA_DIRECTORY_PREFIX}{script_name.replace(".py","")}')
 		return _
 	
@@ -121,6 +152,12 @@ using locals() which does exactly that.''')
 		If `script_name` is `'this script'` (case unsensitive) then it 
 		checks for the current script without the need to hardcode its 
 		name.
+		
+		Parameters
+		----------
+		script_name: str
+			Name of the script you want to check if it was run (and if it
+			was whether it was without errors) on the current measurement.
 		"""
 		if script_name.lower() == 'this script':
 			script_name = str(self._processing_script_absolute_path.parts[-1])
@@ -130,12 +167,17 @@ using locals() which does exactly that.''')
 			return (self.processed_by_script_dir_path(script_name)/Path(self.SCRIPT_SUCCESSFULLY_FINISHED_WITHOUT_ERRORS_FILE_FLAG_NAME)).is_file()
 	
 	def verify_no_errors_context(self):
-		"""Call with a context manager to create a hidden file if the job is completed without errors so later on other scripts can verify this. Example:
+		"""Call with a context manager to create a hidden file if the job
+		is completed without errors so later on other scripts can verify 
+		this. Example:
 		```
 		with buraucrat.veriry_no_errors_context:
 			very_important_function()
 		```
-		If no errors occur within the `with` block, a file will be created marking the completion of the tasks with no errors."""
+		If no errors occur within the `with` block, a file will be created
+		indicating the completion of the tasks with no errors. Later on
+		you can call `job_successfully_completed_by_script` and it will
+		tell you if there were errors or not."""
 		return _MarkJobWithNoErrors(self.this_script_job_succesfully_completed_flag_file_path)
 	
 class _MarkJobWithNoErrors:
